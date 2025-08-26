59a3d8dad9d13044e38941f98382cdbff9fa6b2d
djexo-tech <ifaqideas@gmail.com>
2025-08-25 23:52:47 -0400

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "GAARA~XMD~" + string_session;
+                        let md = "HUNTER~XMD~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*Hey there, HUNTER-XD User!* 👋🏻
 
