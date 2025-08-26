e445b7f6510aafbc3f2ea96aefb1f41e1db6c789
djexo-tech <ifaqideas@gmail.com>
2025-08-25 23:13:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
+
 const express = require('express');
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
+__path = process.cwd();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+let server = require('./qr');
+let code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
+
 app.use('/server', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Sert le HTML
+app.get('/pair.html', (req, res) => {
+  res.sendFile(__path + '/pair.html');
+});
+
+// Utilise le backend pair.js
+app.use('/pair', code);
+
+app.get('/qr', async (req, res) => {
+  res.sendFile(__path + '/qr.html');
+});
+
+app.get('/', async (req, res) => {
+  res.sendFile(__path + '/main.html');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-    console.log(`
+  console.log(`
 Don't Forgot To Give Star HUNTER-XMD
 
- Server running on http://localhost:` + PORT)
-})
+Server running on http://localhost:` + PORT);
+});
 
-module.exports = app
-  
+module.exports = app;
