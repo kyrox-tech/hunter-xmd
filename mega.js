90550bed3f66a0d77d078fd231e9cea47e4633e7
djexo-tech <ifaqideas@gmail.com>
2025-08-27 10:40:52 -0400

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,1 +1,30 @@
+require('dotenv').config();
+const mega = require('megajs');
 
+const EMAIL = process.env.MEGA_EMAIL;
+const PASSWORD = process.env.MEGA_PASSWORD;
+
+async function upload(fileStream, fileName) {
+  return new Promise((resolve, reject) => {
+    const storage = mega.storage({ email: EMAIL, password: PASSWORD });
+
+    storage.on('ready', () => {
+      const file = storage.upload({ name: fileName });
+      fileStream.pipe(file);
+
+      file.on('complete', () => {
+        resolve(file.link);
+      });
+
+      file.on('error', (err) => {
+        reject(err);
+      });
+    });
+
+    storage.on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+module.exports = { upload };
