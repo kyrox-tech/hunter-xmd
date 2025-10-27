61a2f3365083e76ee5a8828f07570d4700004766
kyrox-tech <sebastienhyppolitekarlens@gmail.com>
2025-10-26 20:58:50 -0400

    Refactor index.js for clarity and performance
    Refactor variable naming and update max listeners.

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const _path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+let server = require('./qr');
+let code = require('./pair');
 const { sessionID } = require('./config');
-require('events').EventEmitter.defaultMaxListeners = 500;
+require('events').defaultMaxListeners = 15;
+
 app.use('/server', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+app.use('/pair', async (req, res, next) => {
+  res.sendFile(_path + '/pair.html');
+});
+app.use('/qr', async (req, res, next) => {
+  res.sendFile(_path + '/qr.html');
+});
+app.use('/', async (req, res, next) => {
+  res.sendFile(_path + '/main.html');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star HUNTER-XMD
+  console.log("Server running on http://localhost:" + PORT);
+});
 
- Server running on http://localhost:` + PORT)
-})
+module.exports = app;
 
-module.exports = app
-  
